43bd6121d7130bd8e60d13cf45c7fa446e50792e
Christian <christian@anysrc.net>
2020-02-15 11:19:41 +0100

    Update func.php

--- src/func.php
+++ src/func.php
@@ -2,31 +2,31 @@
 
 function cleanAlias($alias)
 {
-	$clean = preg_replace('/[^A-Za-z0-9\-\_]/', '_', $alias);
-	while(strpos($clean, "__") !== false)
-	{
-		$clean = str_replace("__", "_", $clean);
-	}
-	
-	$clean = trim($clean, "_");
-	
-	if (empty($clean))
-	{
-		return null;
-	}
-	
-	return $clean;
+    $clean = preg_replace('/[^A-Za-z0-9\-\_]/', '_', $alias);
+    while(strpos($clean, "__") !== false)
+    {
+        $clean = str_replace("__", "_", $clean);
+    }
+    
+    $clean = trim($clean, "_");
+    
+    if (empty($clean))
+    {
+        return null;
+    }
+    
+    return $clean;
 }
 
 function cleanUrl($url)
 {
-	$temp = trim($url);
-	
-	if (preg_match('/^http(s)?:\/\//', $temp) !== 1)
-	{
-		return null;
-	}
+    $temp = trim($url);
+    
+    if (preg_match('/^http(s)?:\/\//', $temp) !== 1)
+    {
+        return null;
+    }
   
-	$temp = preg_replace('/["<>]/', "", $temp);	
-	return $temp;
+    $temp = preg_replace('/["<>]/', "", $temp);    
+    return $temp;
 }
